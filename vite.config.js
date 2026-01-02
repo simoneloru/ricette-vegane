473683c7feb3ec3345fc289773dd9daf9f33f289
Simone Loru <simone.loru@jiarasolutions.com>
2026-01-02 14:05:39 +0100

    Config: Setup custom domain ricette.simoneloru.com

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ricette-vegane/'
+  // Changed to '/' because for a custom domain (ricette.simoneloru.com) the site lives at the root
+  base: '/'
 })
